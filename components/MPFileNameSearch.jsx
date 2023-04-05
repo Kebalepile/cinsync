3e1be7626ade7d00845978b008699cdb3167edc6
Kebalepile <pimpabutterfly86@gmail.com>
2023-04-05 23:33:36 +0200

    made code more appropriate for a Next.js React component, replaced the document.createElement calls with React elements, and manage the list of suggestions in the component's state.

--- components/MPFileNameSearch.jsx
+++ components/MPFileNameSearch.jsx
@@ -1,57 +1,26 @@
-import React, { Fragment, useContext, useRef } from "react";
+import React, { Fragment, useContext, useState } from "react";
 import MPFileContext from "@/contexts/media/context";
 import sanitizeInput from "@/library/sanitizeInput";
 export default () => {
   const { mpFileNames, LoadFile } = useContext(MPFileContext);
-
-  const suggestionsRef = useRef(null);
+  const [suggestions, setSuggestions] = useState([]);
+  
   const handleSubmit = (e) => {
     e.preventDefault();
-    let res = mpFileNames.searchByName(
+    let results = mpFileNames.searchByName(
       sanitizeInput(e.target.querySelector('input[name="search"]').value)
     );
-    handleSuggestions(res);
+    setSuggestions(results);
   };
   const handleInputChange = (e) => {
     if (e.target.value.length >= 3) {
-      let res = mpFileNames.searchByName(sanitizeInput(e.target.value));
-
-      handleSuggestions(res);
+      let results = mpFileNames.searchByName(sanitizeInput(e.target.value));
+      setSuggestions(results);
     } else {
-      clearSuggestions();
+      setSuggestions([])
     }
   };
-  const handleSuggestions = (results) => {
-    const elem = suggestionsRef.current;
-
-    if (results.length) {
-      elem.textContent = ``;
-      for (let result of results) {
-        let div = document.createElement("div");
-        div.onclick = e => {
-          LoadFile(
-            e.target.getAttribute("data-name"),
-            e.target.getAttribute("data-id")
-          );
-        }
-        div.setAttribute('data-id',result.id)
-        div.setAttribute('data-name',result.name)
-        div.textContent = result.name;
-        elem.appendChild(div);
-      }
-    } else {
-      noSuchFileAlert()
-    }
-  };
-  const clearSuggestions = () => {
-    suggestionsRef.current.textContent = ``;
-  };
-  const noSuchFileAlert = () => {
-    suggestionsRef.current.textContent = "No such media file!"
-    setTimeout(() => {
-      clearSuggestions()
-    }, 1000)
-  }
+ 
   return (
     <Fragment>
       <form id="search_file_name" onSubmit={handleSubmit}>
@@ -64,7 +33,17 @@ export default () => {
           required
         />
         <input type="submit" value="Look" />
-        <article ref={suggestionsRef}></article>
+      
+        {suggestions.map((result) => {
+          return <div
+            key={result.id}
+            onClick={() => {
+              LoadFile(result.name, result.id);
+            }}
+          >
+            {result.name}
+          </div>;
+        })}
       </form>
     </Fragment>
   );
