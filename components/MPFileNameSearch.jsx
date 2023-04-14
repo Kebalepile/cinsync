c6a42ca3cf501897ecd1a63cd3d7b6678fb16b80
Kebalepile <pimpabutterfly86@gmail.com>
2023-04-14 18:28:32 +0200

    file name form css styling

--- components/MPFileNameSearch.jsx
+++ components/MPFileNameSearch.jsx
@@ -1,5 +1,6 @@
 import React, { Fragment, useContext, useState } from "react";
 import MPFileContext from "@/contexts/media/context";
+import styles from "@/styles/mpfilenamesearch.module.css"
 import sanitizeInput from "@/library/sanitizeInput";
 export default () => {
   const { mpFileNames, LoadFile } = useContext(MPFileContext);
@@ -23,16 +24,16 @@ export default () => {
  
   return (
     <Fragment>
-      <form id="search_file_name" onSubmit={handleSubmit}>
+      <form className={styles.searchName} onSubmit={handleSubmit}>
         <input
           type="text"
           name="search"
           minLength={3}
           onChange={handleInputChange}
-          placeholder="type media file name."
+          placeholder="type file name."
           required
         />
-        <input type="submit" value="Look" />
+        <input type="submit" value="Go" />
       
         {suggestions.map((result) => {
           return <div
