3881ddb52e4f4a9a6f217e6d53087292b1ec9189
Kebalepile <pimpabutterfly86@gmail.com>
2023-04-15 21:20:08 +0200

    dynamic navigation with regex

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,18 +1,33 @@
-
 import Link from "next/link";
+import { useRouter } from "next/router";
 import styles from "@/styles/nav.module.css";
-import LoadMPFiles from "@/components/LoadMPFiles"
+import LoadMPFiles from "@/components/LoadMPFiles";
+
 export default () => {
-  
+  const router = useRouter(),
+    currentPath = router.pathname;
+    const isHome = /^\//.test(currentPath),
+    isAbout = /^\/about/.test(currentPath),
+    isFiles = /^\/files/.test(currentPath);
   return (
     <nav className={styles.navigation}>
-      <Link className={`${styles.logo} ${styles.center}`} href="/">Logo</Link>
-      <div id={styles["media-type"]}>
-        <LoadMPFiles/>
-      </div>
-      {/* <Link href="/files">Mp file search</Link> */}
-      
-      <Link className={`${styles.about} ${styles.center}`} href="/about">About</Link>
+      {isHome && <Link className={`${styles.logo} ${styles.center}`} href="/">
+        Logo
+      </Link>}
+      {isFiles && (
+        <div className={styles["mediafiles"]}>
+          <LoadMPFiles />
+        </div>
+      )}
+      {
+        (!isFiles && <Link href="/files" className={styles["mediafiles"]}>media</Link>)}
+
+      {
+        (!isAbout && (
+          <Link className={`${styles.about} ${styles.center}`} href="/about">
+            About
+          </Link>
+        ))}
     </nav>
   );
 };
