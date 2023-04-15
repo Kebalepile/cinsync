bfefff010457a5bf0de32d85ffe65be00e571e4b
Kebalepile <pimpabutterfly86@gmail.com>
2023-04-15 20:02:59 +0200

    removed uneccessary imports and code in 'files.js'

--- pages/files.js
+++ pages/files.js
@@ -1,13 +1,11 @@
-// import LoadMPFiles from "@/components/LoadMPFiles";
+
 import MPPlayList from "@/components/MPPlayList";
 import MPFilePlayer from "@/components/MPFilePlayer";
 function file() {
   return (
     <>
-      {/* <LoadMPFiles /> */}
       <MPFilePlayer />
       <MPPlayList />
-      
     </>
   );
 }
