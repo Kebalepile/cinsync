9d44d6269c3cde1c1086781e3519b5b23750bf13
Kebalepile <pimpabutterfly86@gmail.com>
2023-04-19 20:38:47 +0200

    updated sanitized regex

--- library/sanitizeInput.js
+++ library/sanitizeInput.js
@@ -1,14 +1,11 @@
 /**
- * 
- * @param {*} input 
+ *
+ * @param {*} input
  * @description Uses regular expression to remove any HTML tags from input, then remove any charecters
  * that are commonly used in code injections.
- * @returns 
+ * @returns
  */
 export default function sanitizeInput(input) {
-  // remove any HTML tage.
-  input = input.replace(/(<([^>]+)>)/gi, "");
-  // remove any charectoers that chould be used in code injection.
-  input = input.replace(/[&<>"'`=\/]/g, "");
-  return input;
+  let sanitizedText = input.replace(/<[^>]+>|[&<>"'\/=\\^[\]{}+(),]/g, "");
+  return sanitizedText;
 }
