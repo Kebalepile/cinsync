5c238a9e54afbda77c76793e94ff7e99a4e48aea
Kebalepile <pimpabutterfly86@gmail.com>
2023-04-25 09:44:40 +0200

    compile component css animation stying

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
 import styles from "@/styles/home.module.css";
-
+import Compile from "@/components/Compile"
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
@@ -24,7 +24,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        {/* <h1> welcome home</h1> */}
+        <Compile/>
       </main>
     </>
   );
