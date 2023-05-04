39fc16d29e75599919b651bb7d6f721ef095dacb
Kebalepile <pimpabutterfly86@gmail.com>
2023-05-04 15:47:14 +0200

    more css styling for mp4 media player

--- components/MP4Player.jsx
+++ components/MP4Player.jsx
@@ -148,7 +148,7 @@ export default () => {
             ref={autoPlayRef}
             className={styles.autoPlayBtn}
           >
-            autoPlay
+            auto
           </div>
 
           <TbPictureInPictureOn
